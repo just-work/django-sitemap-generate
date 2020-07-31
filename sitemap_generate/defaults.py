efc02d2d25222d082f832a64444c31d58a6b7e02
kpy21ui <kpy21ui@gmail.com>
2020-07-31 11:35:31 +0300

    Remove casting for `SITEMAP_PORT` variable

--- sitemap_generate/defaults.py
+++ sitemap_generate/defaults.py
@@ -3,6 +3,6 @@ from os import getenv as e
 # Hostname used in sitemap links
 SITEMAP_HOST = e('SITEMAP_HOST', 'localhost')
 # Port used in sitemap links
-SITEMAP_PORT = str(e('SITEMAP_PORT', 443))
+SITEMAP_PORT = e('SITEMAP_PORT', '443')
 # Protocol used in sitemap links
 SITEMAP_PROTO = e('SITEMAP_PROTO', 'https')
